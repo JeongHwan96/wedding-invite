4f2d34cf56b20af0aff3db110788af0b51610a5d
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-06-13 13:56:49 +0900

    Add Tmap API

--- src/components/Map.ts
+++ src/components/Map.ts
@@ -6,7 +6,8 @@ import Component from "vue-class-component";
 export default class KakaoMap extends Vue {
   private map: any = null; // kakao.maps.Map 타입은 any로 둠
   private marker: any = null;
-  destination = encodeURIComponent("드마레웨딩컨벤션");
+  urlComponent: string = "드마레웨딩컨벤션";
+  destination = encodeURIComponent(this.urlComponent);
   url = "tmap://route?goalname =${destination}&goalx=${long}&goaly=${lat}&appname=JKNaviation";
   lat = 36.99430453447451;
   lon = 127.08792010448062;
