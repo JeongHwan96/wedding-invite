b0e3356c7536645216ea074c26dc225e845d0887
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-06-16 13:43:00 +0900

    Add Animation Effect At Main-info.vue

--- src/components/Main_Info.ts
+++ src/components/Main_Info.ts
@@ -2,5 +2,24 @@ import { Vue, Component } from "vue-property-decorator";
 
 @Component
 export default class MainInfo extends Vue {
-  // 컴포넌트 로직
+  mounted() {
+    window.addEventListener("scroll", this.checkPosition);
+  }
+  beforeDestory() {
+    window.removeEventListener("scroll", this.checkPosition);
+  }
+  checkPosition() {
+    const element = document.querySelector(".container");
+
+    if (element) {
+      console.log("현재위치:", window.scrollY);
+
+      if (window.scrollY >= 250 && !element.classList.contains("active")) {
+        console.log("Add class");
+        element.classList.add("active");
+      } else if (window.scrollY < 150 && element.classList.contains("active")) {
+        element.classList.remove("active");
+      }
+    }
+  }
 }
