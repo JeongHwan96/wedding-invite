dc7c848949eb7a911eafc2de19c09438495b6607
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-06-20 16:19:41 +0900

    change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,8 @@
   <footer class="footer_wrap">
     <!-- 테마 버튼 -->
     <div class="center">
-      <div class="copyrights">Designed by Jeong Hwan</div>
-      <div class="copyrights">© 2025 Jeong Hwan Kim All rights reserved.</div>
+      <div class="copyrights">Designed by Jeoung Hwan</div>
+      <div class="copyrights">© 2025 Jeoung Hwan Kim All rights reserved.</div>
     </div>
   </footer>
 </template>
