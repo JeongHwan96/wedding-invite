68259b3c6890135808c0bd3e5f2cc2b925a36f3c
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-06-20 11:53:45 +0900

    change

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,6 +4,24 @@
       <p>오시는 길</p>
     </div>
     <div id="map" class="map"></div>
+    <div class="location__map-icon-box">
+      <div class="location__map-item">
+        <img
+          src="../assets/img/tmap.png"
+          @click="startTmap"
+          class="location__map-icon"
+          alt="naverMap"
+        /><span>TMap</span>
+      </div>
+      <div class="location__map-item">
+        <img
+          src="../assets/img/kakaonav.png"
+          @click="startKakaoMap"
+          class="location__map-icon"
+          alt="kakaoMap"
+        /><span>카카오네비</span>
+      </div>
+    </div>
     <div class="change-area" id="img_tmap_prev" change-data="img"></div>
     <div class="location-trans-wrapper">
       <h3>🚇 지하철</h3>
@@ -15,7 +33,7 @@
       <h3 class="spacing">📍 주소</h3>
       <p>경기도 평택시 중앙로 46 M프라자 9층(평택동 35-3)</p>
       <p>☎️ Tel. 031-650-8200</p>
-      <div class="nav_wrap">
+      <!-- <div class="nav_wrap">
         <div class="navImg_Wrap">
           <img
             id="img_tmap_prev_img"
@@ -32,7 +50,7 @@
             />
           </div>
         </div>
-      </div>
+      </div> -->
 
       <h3 class="spacing">🅿️ 주차</h3>
       <p>건물 주차장 또는 건너편 자란 공영주차장 이용</p>
