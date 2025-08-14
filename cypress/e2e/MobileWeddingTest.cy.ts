8757a0d3292a1103e172a1fd0409afc48b986192
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-08-14 16:35:24 +0900

    Add Mobile Wedding Cypress seq

--- cypress/e2e/MobileWeddingTest.cy.ts
+++ cypress/e2e/MobileWeddingTest.cy.ts
@@ -1,7 +1,26 @@
-import { transform } from "cypress/types/lodash";
-
 describe("MobileWedding Test", () => {
-  it("Main Page 접속", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("scroll 동작 확인", () => {
+    cy.get("#app");
+    cy.scrollTo("bottom");
+    cy.wait(1000);
+    cy.scrollTo("top");
+  });
+
+  it("scroll 동작 후 효과 확인", () => {
+    cy.scrollTo(0, 300);
+    cy.get(".container")
+      .should("have.class", "active")
+      .invoke("css", "opacity")
+      .should("match", /1/);
+    cy.wait(200);
+    cy.scrollTo("top");
+    cy.get(".container")
+      .should("not.have.class", "active")
+      .invoke("css", "opacity")
+      .should("match", /0/);
   });
 });
