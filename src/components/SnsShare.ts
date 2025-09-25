4a753117fb2147a638d131489b7facbb61efee8e
김정환/(협력사) 주임연구원/EV충전SW개발Project <jeonghwan12.kim@lge.com>
2025-09-25 10:17:37 +0900

    Change kakao Description

--- src/components/SnsShare.ts
+++ src/components/SnsShare.ts
@@ -25,7 +25,8 @@ export default class SnsShare extends Vue {
       objectType: "feed",
       content: {
         title: "결혼식에 초대합니다 💌",
-        description: "이제 한 가족이 되려합니다 귀한 걸음으로 저희의 시작을 축복해주세요!",
+        description:
+          "두 사람이 소중한 인연을 맺고 함께 걸어가려 합니다. 귀한 걸음으로 저희의 시작을 축복해주세요!",
         imageUrl: "https://mwedding-invite.netlify.app/assets/img/img1.jpg",
         link: {
           mobileWebUrl: "https://mwedding-invite.netlify.app/",
