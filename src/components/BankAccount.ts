db23385eada480df19e41cf9fe56ac0f8ccb7cb1
Jeong Kimg <kosy17@naver.com>
2026-03-05 23:41:47 +0900

    change bank account number

--- src/components/BankAccount.ts
+++ src/components/BankAccount.ts
@@ -2,12 +2,71 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class BankAccount extends Vue {
-  acc_w: boolean = false;
-  isClicked: boolean = false;
-  groom: number = 12345678;
-  bride: number = 12345678;
-  groom_father: number = 12345678;
-  groom_mother: number = 12345678;
-  bride_father: number = 12345678;
-  bride_mother: number = 12345678;
+  public acc_w = false;
+  public isClicked = false;
+
+  public groom = "110-439-592371";
+  public bride = "3333-13-3977466";
+  public groom_father = "115-266802-12004";
+  public groom_mother = "113-12-822921";
+  public bride_father = "432-02-056303";
+  public bride_mother = "425-02-360598";
+
+  public handleModal(event?: MouseEvent): void {
+    this.isClicked = !this.isClicked;
+
+    if (event && event.target) {
+      const target = event.target as HTMLElement;
+      if (target.id === "acc_w") {
+        this.acc_w = true;
+      } else if (target.id === "acc_m") {
+        this.acc_w = false;
+      }
+    }
+  }
+
+  public copyInfo(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const buttonId = target.id;
+    let textToCopy = "";
+
+    switch (buttonId) {
+      case "groom-father":
+        textToCopy = this.groom_father;
+        break;
+      case "groom-mother":
+        textToCopy = this.groom_mother;
+        break;
+      case "groom":
+        textToCopy = this.groom;
+        break;
+      case "bride-father":
+        textToCopy = this.bride_father;
+        break;
+      case "bride-mother":
+        textToCopy = this.bride_mother;
+        break;
+      case "bride":
+        textToCopy = this.bride;
+        break;
+    }
+
+    if (textToCopy) {
+      const cleanText = textToCopy.replace(/-/g, "");
+
+      this.executeCopy(cleanText);
+      alert("복사되었습니다.");
+    }
+
+    this.isClicked = false;
+  }
+
+  private executeCopy(text: string): void {
+    const t = document.createElement("textarea");
+    document.body.appendChild(t);
+    t.value = text;
+    t.select();
+    document.execCommand("copy");
+    document.body.removeChild(t);
+  }
 }
--- src/components/BankAccount.vue
+++ src/components/BankAccount.vue
@@ -9,7 +9,7 @@
               <div class="divide"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>농협 12345678</li>
+                  <li>우리은행 {{ groom_father }}</li>
                   <li>예금주: 김태경</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="groom-father">복사하기</button>
@@ -17,7 +17,7 @@
               <div class="padding"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>농협 1234567</li>
+                  <li>농협은행 {{ groom_mother }}</li>
                   <li>예금주: 이희경</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="groom-mother">복사하기</button>
@@ -27,7 +27,7 @@
               <div class="divide"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>신한 110439592371</li>
+                  <li>신한은행 {{ groom }}</li>
                   <li>예금주: 김정환</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="groom">복사하기</button>
@@ -41,7 +41,7 @@
               <div class="divide"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>농협 1234567</li>
+                  <li>농협은행 {{ bride_father }}</li>
                   <li>예금주: 박권서</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="bride-father">복사하기</button>
@@ -49,7 +49,7 @@
               <div class="padding"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>농협 1234567</li>
+                  <li>농협은행 {{ bride_mother }}</li>
                   <li>예금주: 이채민</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="bride-mother">복사하기</button>
@@ -59,7 +59,7 @@
               <div class="divide"></div>
               <div class="account-row">
                 <div class="account-row-column-text">
-                  <li>카카오뱅크 3333133977466</li>
+                  <li>카카오뱅크 {{ bride }}</li>
                   <li>예금주: 박소연</li>
                 </div>
                 <button class="btn-copy" @click="copyInfo" id="bride">복사하기</button>
@@ -81,61 +81,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      acc_w: false,
-      isClicked: false,
-      groom: "110439592371",
-      bride: "3333133977466",
-      groom_m: "86501117977",
-      groom_w: "82108656096936",
-      bride_w: "13004256015272"
-    };
-  },
-  methods: {
-    handleModal(event) {
-      this.isClicked = !this.isClicked;
-      const id = event.target.id;
-      if (id == "acc_w") {
-        this.acc_w = true;
-      } else this.acc_w = false;
-    },
-    copyInfo(event) {
-      const buttonId = event.target.id;
-
-      const t = document.createElement("textarea");
-      document.body.appendChild(t);
-
-      if (buttonId == "groom-m") {
-        t.value = this.groom_m;
-        t.select();
-        document.execCommand("copy");
-      } else if (buttonId == "groom-w") {
-        t.value = this.groom_w;
-        t.select();
-        document.execCommand("copy");
-      } else if (buttonId == "groom") {
-        t.value = this.groom;
-        t.select();
-        document.execCommand("copy");
-      } else if (buttonId == "bride-w") {
-        t.value = this.bride_w;
-        t.select();
-        document.execCommand("copy");
-      } else {
-        // this.$copyText(this.bride);
-        t.value = this.bride;
-        t.select();
-        document.execCommand("copy");
-      }
-      alert("복사되었습니다.");
-      document.body.removeChild(t);
-      this.handleModal();
-    }
-  }
-};
-</script>
+<script lang="ts" src="./BankAccount.ts"></script>
 
 <style scoped src="../assets/css/BankAccount.css"></style>
